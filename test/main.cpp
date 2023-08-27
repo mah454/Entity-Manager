7177a4cfd21adb6b1b0b9d34bcf82a80d5c5fc9d
mah454 <mah454@gmail.com>
2023-08-27 20:25:56 +0330

    Improvement Code

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,9 @@
-
-#include <vector>
-#include "repository/PersonRepository.h"
 #include <syslog.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
-
     openlog("entity-manager", LOG_PID, LOG_LOCAL0);
-
     ::testing::InitGoogleTest(&argc, argv);
-
     return RUN_ALL_TESTS();
 }
 
